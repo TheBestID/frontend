8ed3366bd3f30b1dd7dd2bb13d6b25dddd3bc4f5
VinogradovDanya <vinogradov.do@phystech.edu>
2022-11-06 16:09:59 +0300

    change opacity

--- pages/profile/[profile].tsx
+++ pages/profile/[profile].tsx
@@ -21,7 +21,7 @@ const Profile: NextPage = (props) => {
         <meta name="description" content="add wallet to register on souldev platform" />
       </Head>
 
-      <header className="flex bg-[#023047] h-24 w-full lg:px-16 justify-between pt-4">
+      <header className="fixed flex bg-[#023047] h-24 w-full lg:px-16 justify-between pt-4">
 
       <div className="flex">
         <div className ="h-16 w-16 ml-2">
@@ -50,7 +50,7 @@ const Profile: NextPage = (props) => {
 
       </header>  
 
-      <main className="flex items-center flex-col mr-2 ml-2">
+      <main className="flex items-center flex-col mr-2 ml-2 pt-14">
 
         <div className="rounded-md w-full bg-primary h-44 mt-12 mb-12">
           <img src="/download.svg" alt="Identicon" className="rounded-full h-32 w-32 mx-5 my-20"/>
@@ -59,8 +59,8 @@ const Profile: NextPage = (props) => {
         <div className=" flex w-full text-left text-2xl text-white mb-6">DANILA</div>
 
         <div className="border w-full h-12 border-primary p-1 rounded-xl pr-3 pl-3 pb-3 pt-3">
-          <span className="text-white opacity-60 ">Address</span>
-          <span className="text-white opacity-60 px-14"> {wallet}</span>
+          <span className="text-[#fff8] ">Address</span>
+          <span className="text-[#fff8] px-14">{wallet}</span>
         </div>
 
         <button className="rounded-xl w-32 h-12 text-white bg-secondary-25 mt-6">
