bbef472b73abf80d99ff6597745349c6b6e3e43f
notTGY <microcraft47@gmail.com>
2022-11-13 19:35:31 +0300

    burger

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,63 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
 type Props = {}
 
 const Header: React.FC<Props> = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
     <header
-      className="border-b border-primary fixed flex bg-[#023047] h-24 w-full lg:px-16 justify-between pt-4 z-10"
+      className="border-b border-primary fixed bg-[#023047] w-full z-10"
     >
-
-      <Link href="/">
-        <a className="flex">
-          <div className ="h-16 w-16 ml-2">
-            <div
-              className="bg-primary rounded-full h-16 w-16"
-            >
-              <Image alt="logo" src="/logo.svg" width="64" height="64"/>
+      <div className="flex h-24 w-full lg:px-16 justify-between pt-4">
+        <Link href="/">
+          <a className="flex">
+            <div className ="h-16 w-16 ml-2">
+              <div
+                className="bg-primary rounded-full h-16 w-16"
+              >
+                <Image alt="logo" src="/logo.svg" width="64" height="64"/>
+              </div>
             </div>
+
+            <div
+              className="flex flex-col ml-2 h-16 justify-center"
+            >
+              <div
+                className="text-white font-bold text-3xl"
+              >
+                Souldev
+              </div>
+              <div
+                className="font-semibold tracking-[0.55em] text-[#219EBC]"
+              >
+                network
+              </div>
+            </div>
+          </a>
+        </Link>
+
+        <button
+          className="flex"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+
+          <div className ="h-12 w-12 pt-2 pr-12">
+            <div
+              className="bg-primary rounded-full h-12 w-12"
+            />
+          </div> 
+
+          <div className="space-y-1 pt-4 ml-2 mr-2">
+            <div
+              className="bg-primary rounded-full h-2 w-12"
+            />
+            <div
+              className="bg-primary rounded-full h-2 w-12"
+            />
+            <div
+              className="bg-primary rounded-full h-2 w-12"
+            />
           </div>
 
-          <div
-            className="flex flex-col ml-2 h-16 justify-center"
-          >
-            <div
-              className="text-white font-bold text-3xl"
-            >
-              Souldev
-            </div>
-            <div
-              className="font-semibold tracking-[0.55em] text-[#219EBC]"
-            >
-              network
-            </div>
-          </div>
-        </a>
-      </Link>
-
-      <div className="flex">
-
-        <div className ="h-12 w-12 pt-2 pr-12">
-          <div
-            className="bg-primary rounded-full h-12 w-12"
-          />
-        </div> 
-
-        <div className="space-y-1 pt-4 ml-2 mr-2">
-          <div
-            className="bg-primary rounded-full h-2 w-12"
-          />
-          <div
-            className="bg-primary rounded-full h-2 w-12"
-          />
-          <div
-            className="bg-primary rounded-full h-2 w-12"
-          />
-        </div>
-
+        </button>
       </div>
+
+      { isMenuOpen && (
+        <div className="h-10 w-full transition-all duration-500 ">
+          hi
+        </div>
+      )}
 
     </header>  
   )
