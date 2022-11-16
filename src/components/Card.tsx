f2bffe5a7d1262e62255d9828e237346bdc384b6
VinogradovDanya <vinogradov.do@phystech.edu>
2022-11-16 18:29:52 +0300

    updrate card in main page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,14 +15,14 @@ const Card: React.FC<Props> = (props) => {
     <Link href={href}>
       <a>
         <div className="border border-primary p-1 rounded-md pr-3 pl-3 pb-3 pt-3">
-          <div className="bg-primary rounded-md h-24 w-full flex"/>
+          <div className="bg-primary rounded-md h-40 w-full flex"/>
 
           <div className="justify-center w-full flex relative top-[-3rem]">
-            <div className="flex w-24 aspect-square rounded-full bg-black"/>
+            <div className="flex w-28 aspect-square rounded-full bg-black"/>
           </div>
 
           <div className="text-center">
-            <span className="text-white">
+            <span className="text-xl font-medium text-white">
               {title}
             </span>
           </div>
