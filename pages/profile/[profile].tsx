646294478400c8930b3b1137fab33fb9e8ab8342
VinogradovDanya <vinogradov.do@phystech.edu>
2022-11-16 19:12:55 +0300

    add border-2 in new wallet

--- pages/profile/[profile].tsx
+++ pages/profile/[profile].tsx
@@ -136,7 +136,7 @@ const Profile: NextPage<Props> = (props) => {
             <span className="text-[#fff8]">Address</span>
             <span className="text-[#fff8] px-14 underline">{wallet}</span>
           </a>
-          <button className="h-12 w-1/5 p-2 rounded-xl border border-primary hover:bg-primary font-medium items-center justify-center">
+          <button className="h-12 w-1/5 p-2 rounded-xl border-2 border-primary hover:bg-primary font-medium items-center justify-center">
             <div className="text-center">
                <span className="text-white text-xl">Add wallet</span>
             </div>
